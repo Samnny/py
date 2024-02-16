ad1097085b85799ced710c7d07102bd6b8af44df
Samnny <arolllysamay@gmail.com>
2024-02-16 20:12:03 -0300

    first commit

---
+++ q2.py
@@ -0,0 +1,14 @@
+def verifica_fibonacci(numero):
+    a, b = 0, 1
+    while a <= numero:
+        if a == numero:
+            return True
+        a, b = b, a + b
+    return False
+
+numero_informado = int(input("Informe um número: "))
+
+if verifica_fibonacci(numero_informado):
+    print(f"O número {numero_informado} pertence à sequência de Fibonacci.")
+else:
+    print(f"O número {numero_informado} não pertence à sequência de Fibonacci.")
